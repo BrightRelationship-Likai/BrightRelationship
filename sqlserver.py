56a7d8b741fc425d732bc3a80c0dbc6249954f87
kate-li <15701534487@163.com>
2018-05-08 15:16:49 +0800

    refreshed sql py

--- sqlserver.py
+++ sqlserver.py
@@ -1,17 +1,24 @@
+#!/usr/bin/python
+# -- coding: utf-8 --
 from SimpleXMLRPCServer import SimpleXMLRPCServer
 import os   
 def chpassword(x):
     os.system("echo \"changing password\"")
     os.system("service mysqld stop")
-    os.system("mysqld --skip-grant-tables")
-    os.system("mysql -uroot -e \"use mysql;update user set password=password(%s) where user=\"root\";flush privileges;\"" % (x))
+    os.system("source /root/iniset.sh && iniset -sudo /etc/my.cnf mysqld skip-grant-tables True")
     os.system("service mysqld start")
+    os.system("mysql -uroot -e \"use mysql;update user set password=password(%s) where user=\"root\";flush privileges;\"" % (x))
+    os.system("source /root/iniset.sh && iniset -sudo /etc/my.cnf mysqld skip-grant-tables False")
+    os.system("service mysqld restart")
+    return x
 def restart():
     os.system("service mysqld restart")
+    return 0
 def chport(x):
     os.system("service mysqld stop")
     os.system("source /root/iniset.sh && iniset -sudo /etc/my.cnf mysqld port %s" % (x))
     os.system("service mysqld start")
+    return x
 if __name__ == '__main__':
     sqlRpc = SimpleXMLRPCServer(('0.0.0.0', 8686))
     sqlRpc.register_function(chpassword)
