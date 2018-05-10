f3e78dfc085d6bb5b20954dd015f2d7e609e16bf
Darcern-likai <15701534487@163.com>
2018-05-10 09:35:13 +0800

    added argv[]

--- chport.py
+++
@@ -1,4 +0,0 @@
-from xmlrpclib import ServerProxy
-if __name__ == '__main__':
-    s = ServerProxy("http://127.0.0.1:8686")
-    print s.chport(4444)
---
+++ chsqlrestart.py
@@ -0,0 +1,5 @@
+import sys
+from xmlrpclib import ServerProxy
+if __name__ == '__main__':
+    s = ServerProxy("http://%s:8686" % (sys.argv[1]))
+    print s.restart()
